840cf7ff6fd1e9180fdf9c32bef39440467f480d
Bizimana <ebizimana@liberty.edu>
2018-10-01 23:01:59 -0400

    I have done a little of the logic the css file is still to small but developing and most of the code is hardcoded

---
+++ colorGame.js
@@ -0,0 +1,39 @@
+var color = [ "rgb(255, 0, 0)",
+              "rgb(255, 255, 0)",
+              "rgb(0, 255, 0)",
+              "rgb(0, 255, 255)",
+              "rgb(0, 0, 255)",
+              "rgb(255, 0, 255)",
+            ]
+var squares = document.querySelectorAll(".square")
+var pickedColor = color[3]
+var colorDisplay = document.getElementById("colorDisplay")
+var messageDisplay = document.querySelector("#message")
+
+colorDisplay.textContent = pickedColor
+
+for (var i = 0; i < squares.length; i++) {
+  // Add initial colors to squares
+  squares[i].style.backgroundColor = color[i]
+
+  //Add click listener to squares
+  squares[i].addEventListener("click", function(){
+    // grav color of clicked squares
+    var clickedColor = this.style.backgroundColor
+    // compare color to pickedColor
+    if(clickedColor === pickedColor){
+      messageDisplay.textContent = "Correct!"
+    } else {
+      this.style.backgroundColor = "#232323"
+      messageDisplay.textContent = "Try Again"
+    }
+  })
+}
+
+function changeColors(color){
+  // looop through all squares
+  for(var i = 0; i<colors.length; i++){
+    
+  }
+  // change each color to match given color
+}
